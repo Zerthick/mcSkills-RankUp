9efc66ab6f25d8bce23591687eb95762791339dd
Zerthick <zerthick@gmail.com>
2018-05-15 22:01:24 -0400

    Add copyright header

--- src/main/java/io/github/zerthick/mcskillsrankup/McSkillsRankUp.java
+++ src/main/java/io/github/zerthick/mcskillsrankup/McSkillsRankUp.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018  Zerthick
+ *
+ * This file is part of mcSkills-RankUp.
+ *
+ * mcSkills-RankUp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mcSkills-RankUp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mcSkills-RankUp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.zerthick.mcskillsrankup;
 
 import com.google.common.collect.ImmutableMap;
@@ -43,10 +62,10 @@ import java.util.concurrent.TimeUnit;
         name = "McSkills-RankUp",
         description = "A rank up companion plugin for mcSkills",
         authors = {
-            "Zerthick"
+                "Zerthick"
         },
         dependencies = {
-            @Dependency(id = "mcskills")
+                @Dependency(id = "mcskills")
         }
 )
 public class McSkillsRankUp {
